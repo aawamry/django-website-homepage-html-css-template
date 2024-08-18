0c278a9288be8b84d0480d4e38142bfe9d9b347b
aawamry <a.awamry@live.com>
2024-08-18 17:04:17 +0300

    Adding Project to Github

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+
+/* ---------- Media Screen Functions for Layout Template ------------ */
+
+function closeMenu(){
+    var navLinks = document.getElementById("navLinks")
+    navLinks.style.right = "-200px";
+
+}
+function openMenu(){
+    var navLinks = document.getElementById("navLinks")
+    navLinks.style.right = "0";
+
+}
